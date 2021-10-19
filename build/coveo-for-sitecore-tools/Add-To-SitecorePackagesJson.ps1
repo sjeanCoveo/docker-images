8af01fc6b31c1ff336d135d31b078df7f046d4cd
Sébastien Jean <sjean@coveo.com>
2021-10-19 16:12:26 -0400

    Add coveo-for-sitecore-tools b3ad9c59d433a8351ff3da19686251823b973c81 author Sébastien Jean <sjean@coveo.com> 1634674346 -0400 committer Sébastien Jean <sjean@coveo.com> 1636115581 -0400
    Added scripts to allow users to build Coveo for Sitecore assets

---
+++ build/coveo-for-sitecore-tools/Add-To-SitecorePackagesJson.ps1
@@ -0,0 +1,56 @@
+[CmdletBinding(SupportsShouldProcess = $true)]
+
+param(
+    [Parameter(Mandatory = $false)]
+    [ValidateNotNullOrEmpty()]
+    [string]$InstallSourcePath,
+    [Parameter()]
+    [string[]]$SitecoreVersion,
+    [Parameter()]
+    [string]$CoveoVersion,
+    [Parameter()]
+    [switch]$IncludeSxa
+)
+
+$parentPath = (get-item $PSScriptRoot).parent.FullName
+$sitecorePackagesPath = "$parentPath\sitecore-packages.json"
+
+$coveoPackageUrl = "https://static.cloud.coveo.com/coveoforsitecore/packages/v$CoveoVersion./Coveo%20for%20Sitecore%2010.0%20$CoveoVersion.scwdp.zip"
+$coveoSxaPackageUrl = "https://static.cloud.coveo.com/coveoforsitecore/packages/v$CoveoVersion/Coveo%20for%20Sitecore%20SXA%2010.1%20$CoveoVersion.zip"
+
+$propertyKey = "Coveo for Sitecore $SitecoreVersion $CoveoVersion.scwdp.zip"
+$propertyValue = [ordered]@{ url = $coveoPackageUrl; hash = ""; }
+
+$sxaPropertyKey = "Coveo for Sitecore SXA $SitecoreVersion $CoveoVersion.scwdp.zip"
+$sxaPropertyValue = [ordered]@{ url = $coveoSxaPackageUrl; hash = ""; }
+
+$json = Get-Content $sitecorePackagesPath | Out-String | ConvertFrom-Json
+
+if ($IncludeSxa) {
+    $sxaPropertyExists = $json.PSobject.Properties.name -match "$sxaPropertyKey"
+
+    if (!$sxaPropertyExists){
+        $json | Add-Member -Type NoteProperty -Name $sxaPropertyKey -Value $sxaPropertyValue
+        $json | ConvertTo-Json | Set-Content $sitecorePackagesPath
+    }
+}
+
+$json = Get-Content $sitecorePackagesPath | Out-String | ConvertFrom-Json
+
+if ($IncludeSxa) {
+    $sxaPropertyExists = $json.PSobject.Properties.name -match "$sxaPropertyKey"
+
+    if (!$sxaPropertyExists){
+        $json | Add-Member -Type NoteProperty -Name $sxaPropertyKey -Value $sxaPropertyValue
+        $json | ConvertTo-Json | Set-Content $sitecorePackagesPath
+    }
+}
+
+$propertyExists = $json.PSobject.Properties.name -match "$propertyKey"
+
+if (!$propertyExists) {
+    $json | Add-Member -Type NoteProperty -Name $propertyKey -Value $propertyValue
+    $json | ConvertTo-Json | Set-Content $sitecorePackagesPath
+}
+
+& "$parentPath\contributing\Sort-SitecorePackagesJson.ps1"
